ec30e1366ea6fbffd149fd26a537a6349d5a65dc
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-11-20 13:23:34 -0800

    changed account type

--- schemas.sql
+++ schemas.sql
@@ -7,8 +7,9 @@ CREATE TABLE IF NOT EXISTS customers(
     date_became_customer date DEFAULT CURRENT_DATE,
     address1 VARCHAR(50) NOT NULL,
     address2 VARCHAR(20) NOT NULL,
+    city VARCHAR(30) NOT NULL,
     state VARCHAR(2) NOT NULL,
-    country VARCHAR(20) NOT NULL DEFAULT 'United States',
+    zipcode VARCHAR(5) NOT NULL,
     email VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
@@ -18,10 +19,10 @@ DROP TABLE IF EXISTS accounts;
 
 CREATE TABLE IF NOT EXISTS accounts(
     id serial NOT NULL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
+    account_type VARCHAR(20) NOT NULL,
     balance numeric NOT NULL,
     customer_id int4 NOT NULL
-)
+);
 
 DROP TABLE IF EXISTS transactions;
 
@@ -29,7 +30,7 @@ CREATE TABLE IF NOT EXISTS transactions(
     id serial NOT NULL PRIMARY KEY,
     description VARCHAR(100),
     amount numeric NOT NULL,
-    transactionType VARCHAR(20) NOT NULL,
+    transaction_type VARCHAR(20) NOT NULL,
     account_id int4 NOT NULL
-)
+);
 
--- seedCustomer.js
+++ seedCustomer.js
@@ -1,13 +1,15 @@
 const axios = require("axios");
 const { faker } = require('@faker-js/faker');
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 15; i++) {
     let user = {
         firstname: faker.name.firstName(),
         surname: faker.name.lastName(),
         address1: faker.address.streetAddress(),
         address2: faker.address.secondaryAddress(),
+        city: faker.address.cityName(),
         state: faker.address.stateAbbr(),
+        zipcode: faker.address.zipCode('#####'),
         email: faker.internet.email(),
         username: faker.internet.userName(),
         password: "password",
